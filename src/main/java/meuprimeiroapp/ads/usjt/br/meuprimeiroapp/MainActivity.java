61ccc85303b4b96173b6be68d2df03029235127a
Douglas Carvalho <douglas.carvalho00@live.com>
2018-12-13 23:27:35 -0200

    Aula 1

---
+++ src/main/java/meuprimeiroapp/ads/usjt/br/meuprimeiroapp/MainActivity.java
@@ -0,0 +1,26 @@
+package meuprimeiroapp.ads.usjt.br.meuprimeiroapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends Activity {
+    public final static String EXTRA_MESSAGE = "br.usjt.ads.meuprimeiroapp.MESSAGE";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
